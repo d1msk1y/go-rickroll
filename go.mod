56bd8cb7c2c99c58b82f6c85c0ae7949d47d1ae9
d1msk1y <exect81@gmail.com>
2023-01-16 21:48:22 +0100

    never gonna give u up

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/d1msk1y/go-rickroll
+
+go 1.19
---
+++ rickroll/roll.go
@@ -0,0 +1,23 @@
+package rickroll
+
+import (
+	"fmt"
+	"os/exec"
+	"runtime"
+)
+
+func RickRoll() {
+	url := "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
+
+	switch runtime.GOOS {
+	case "linux":
+		exec.Command("xdg-open", url).Start()
+	case "windows":
+		exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		exec.Command("open", url).Start()
+	default:
+		fmt.Errorf("unsupported platform")
+	}
+
+}
